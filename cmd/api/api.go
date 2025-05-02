517f2aa7e3c8b9efda17a0510a265492e0ac5bb8
Omkar Nag <knomkarnag24@gmail.com>
2025-05-02 21:54:50 +0530

    initial server setup using go-chi

---
+++ cmd/api/api.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct{ config config }
+type config struct{ addr string }
+
+func (a *application) run(mux http.Handler) error {
+
+	srv := &http.Server{Addr: a.config.addr, Handler: mux, WriteTimeout: time.Second * 30, ReadTimeout: time.Second * 10, IdleTimeout: time.Minute}
+	log.Printf("Server started at %s", a.config.addr)
+	return srv.ListenAndServe()
+}
+
+func (app *application) mount() http.Handler {
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+
+	return r
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/omkar-nag/socialapp
+
+go 1.22.2
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
