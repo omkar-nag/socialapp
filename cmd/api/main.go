b6bc4aa20283e404f4af7130cb52f9ad1a83ce4d
Omkar Nag <knomkarnag24@gmail.com>
2025-05-04 00:53:24 +0530

    postgres db setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/omkar-nag/socialapp/internal/db"
 	"github.com/omkar-nag/socialapp/internal/env"
 	"github.com/omkar-nag/socialapp/internal/store"
 )
@@ -12,14 +13,27 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 		db: dbConfig{
-			addr:         env.GetString("DB_ADDR", ":postgres://user:adminpassword@localhost/social?sslmode=disable"),
+			addr:         env.GetString("DB_ADDR", "postgres://postgres:postgres@localhost/socialnetwork?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
-			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "5min"),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "5m"),
 		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Println("Database established")
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
